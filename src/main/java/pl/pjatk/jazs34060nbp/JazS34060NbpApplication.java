f9f003225c1421fbe3729da2172780958a9729a1
Filip Szlagowski <filip.szlagowski@gmail.com>
2026-06-20 12:18:52 +0200

    pierwszy commit

---
+++ src/main/java/pl/pjatk/jazs34060nbp/JazS34060NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs34060nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS34060NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS34060NbpApplication.class, args);
+    }
+
+}
